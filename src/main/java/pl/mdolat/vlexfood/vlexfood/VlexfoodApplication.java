d0bba03cea136aaee54a469eb10350a442829222
mdolata <mateuszdolata2@gmail.com>
2018-03-28 01:27:58 +0200

    Initial commit

---
+++ src/main/java/pl/mdolat/vlexfood/vlexfood/VlexfoodApplication.java
@@ -0,0 +1,12 @@
+package pl.mdolat.vlexfood.vlexfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlexfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VlexfoodApplication.class, args);
+	}
+}
